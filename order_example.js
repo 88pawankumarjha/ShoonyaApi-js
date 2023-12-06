19903ea03b19bd19cc4bf5dfc453fc6220688968
Pawan Kumar Jha <88pawankumarjha@gmail.com>
2023-12-06 21:14:53 +0000

    order updates understanding in websocket

--- order_example.js
+++ order_example.js
@@ -1,21 +1,23 @@
 const Api = require("./lib/RestApi");
 
-let { authparams } = require("./cred");
+let { authparams } = require("./creds");
 
 api = new Api({});
+let latestOrders = {};
 
 function receiveQuote(data) {
-    console.log("Quote ::", data);
+    // console.log("Quote ::", data);
 }
 
 function receiveOrders(data) {
-    console.log("Order ::", data);
+    // console.log("Order ::", data);
+    latestOrders[data.tsym] = data;
 }
 
 function open(data) {
-    let instruments = 'NSE|22#BSE|500400';
+    let instruments = '';
     api.subscribe(instruments)
-    console.log("subsribing to :: ", instruments);
+    // console.log("subsribing to :: ", instruments);
 }
 
 api.login(authparams)
@@ -35,38 +37,53 @@ api.login(authparams)
             'buy_or_sell' : 'B',
             'product_type' : 'C',
             'exchange' : 'NSE',
-            'tradingsymbol'  :  'CANBK-EQ',
+            'tradingsymbol'  :  'ACC-EQ',
             'quantity' : 1,
             'discloseqty' : 0,
             'price_type' : 'LMT',
-            'price' : 175.0
+            'price' : 2201.0
         };
 
-        api.place_order(orderparams)
+        setTimeout(() => {
+            api.place_order(orderparams)
             .then((reply) => { 
-                console.log(reply);            
+                console.log(reply, reply);   
+                
 
-        let modifyparams = {
-            'orderno' : reply.norenordno,
-            'exchange' : 'NSE',
-            'tradingsymbol' : 'CANBK-EQ',
-            'newquantity' : 2,
-            'newprice_type' : 'LMT',
-            'newprice' : 176.00
-        }
+        // let modifyparams = {
+        //     'orderno' : reply.norenordno,
+        //     'exchange' : 'NSE',
+        //     'tradingsymbol' : 'ACC-EQ',
+        //     'newquantity' : 2,
+        //     'newprice_type' : 'LMT',
+        //     'newprice' : 2202.00
+        // }
 
-        api.modify_order(modifyparams)
-            .then((modreply) => { 
-                    console.log(modreply);
-                    api.cancel_order(modreply.result);
-                });
+        // api.modify_order(modifyparams)
+        //     .then((modreply) => { 
+        //             // console.log(modreply, 'modreply');
+                    
+        //             api.cancel_order(modreply.result)
+        //             .then((cancelreply) => {
+        //                 console.log(cancelreply, 'cancelreply');
+        //             });
+        //             console.log(latestOrders, 'latestOrders2')
+        //         });
         });
+        },2000 )
 
-        api.get_orderbook()
-        .then((reply) => { 
-            console.log(reply);
-        });
+        setTimeout(() => {
+            console.log(latestOrders, 'latestOrders1')
+        }, 4000)
         
+
+        // api.get_orderbook()
+        // .then((reply) => { 
+        //     // console.log(reply);
+        // });
+        
+
+
     }).catch((err) => {
         console.error(err);
     });
