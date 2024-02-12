30208bcc28d6ce4d29932a74946ca5b6dda14029
Pawan Kumar Jha <88pawankumarjha@gmail.com>
2024-02-12 09:17:38 +0000

    added logger

--- utils/common.js
+++ utils/common.js
@@ -367,7 +367,7 @@ const takeDecision = async (api, up, vixQuoteCalc, actionTypeInput) => {
 
     
     pnl = await calcPnL(api);
-    send_notification('PnL : ' + pnl, true)
+    send_notification('Common PnL : ' + pnl, true)
     send_notification('Common PnL : ' + pnl)
     
     //send distance and MtoM
