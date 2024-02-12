58ac3910b47a2a88456075cc29bb5d14499d3e4b
Pawan Kumar Jha <88pawankumarjha@gmail.com>
2024-02-12 09:09:25 +0000

    fast from slow

--- xema.js
+++ xema.js
@@ -1471,8 +1471,8 @@ const emaMonitorATMs = async () => {
     const [callemaMedium, callemaSlow, callemaFast] = await ema9_21_3ValuesIndicators(paramsCall);
     const [putemaMedium, putemaSlow, putemaFast] = await ema9_21_3ValuesIndicators(paramsPut);
     send_notification('cem : ' + parseFloat(callemaMedium ).toFixed(2)+ ' cef : ' + parseFloat(callemaFast).toFixed(2)  + '\npem : ' +parseFloat(putemaMedium ).toFixed(2)+ ' pef : ' +parseFloat(putemaFast).toFixed(2))
-    emaUpFastCall = callemaFast > callemaMedium
-    emaUpFastPut = putemaFast  > putemaMedium
+    emaUpFastCall = callemaMedium > callemaFast
+    emaUpFastPut = putemaMedium  > putemaFast
     return [emaUpFastCall, emaUpFastPut];
   } catch (error) {
     // handle the exception locally
