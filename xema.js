c371fafd6592636e91db7c0fbd39ad207b92113b
Pawan Kumar Jha <88pawankumarjha@gmail.com>
2024-02-23 07:10:45 +0000

    messgae update

--- xema.js
+++ xema.js
@@ -1034,7 +1034,7 @@ const emaMonitorATMs = async () => {
       }
     const [callemaMedium, callemaSlow, callemaFast] = await ema9_21_3ValuesIndicators(paramsCall);
     const [putemaMedium, putemaSlow, putemaFast] = await ema9_21_3ValuesIndicators(paramsPut);
-    send_notification('cem : ' + parseFloat(callemaMedium ).toFixed(2)+ ' cef : ' + parseFloat(callemaFast).toFixed(2)  + '\npem : ' +parseFloat(putemaMedium ).toFixed(2)+ ' pef : ' +parseFloat(putemaFast).toFixed(2))
+    send_notification('cem : ' + parseFloat(callemaMedium ).toFixed(2)+ 'pem : ' +parseFloat(putemaMedium ).toFixed(2)+ '\n cef : ' + parseFloat(callemaFast).toFixed(2) + ' pef : ' +parseFloat(putemaFast).toFixed(2))
     emaUpFastCall = callemaFast > callemaMedium;
     emaUpFastPut = putemaFast > putemaMedium;
     return [emaUpFastCall, emaUpFastPut];
@@ -1213,7 +1213,7 @@ async function takeEMADecision(emaMonitorFastCallUp, emaFastMonitorPutUp) {
       await enterXemaShort()
     }
   }
-    send_notification(biasOutput.bias + ' ' + longPositionTaken + ' ' + shortPositionTaken + " : bias long short")
+    send_notification(biasOutput.bias + ' ' + longPositionTaken ? 'Long' : shortPositionTaken ? 'Short' : 'No Position')
 }
 
 const setBiasValue = async () => {
