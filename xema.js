c9dc79ac89672044c919ee88d1fcad1b4a8bd18e
Pawan Kumar Jha <88pawankumarjha@gmail.com>
2024-03-07 03:25:47 +0000

    nft fin bnf order by dates

--- xema.js
+++ xema.js
@@ -31,8 +31,8 @@ let globalBigInput = {
   filteredIndexCSV: undefined
 }
 //TODO change index
-getPickedIndexHere = () => debug ? 'NIFTY' : ['NIFTY', 'BANKEX', 'FINNIFTY', 'BANKNIFTY', 'NIFTY', 'SENSEX', 'BANKEX'][new Date().getDay()] || 'NIFTY';
-getEMAQtyFor2L = () => debug ? 100 : [100, 60, 120, 60, 150, 50, 100][new Date().getDay()] || 100; // qty for margin to sell both sides
+getPickedIndexHere = () => debug ? 'NIFTY' : ['NIFTY', 'BANKEX', 'NIFTY', 'FINNIFTY', 'BANKNIFTY', 'SENSEX', 'BANKEX'][new Date().getDay()] || 'NIFTY';
+getEMAQtyFor2L = () => debug ? 100 : [100, 60, 150, 120, 60, 50, 100][new Date().getDay()] || 100; // qty for margin to sell both sides
 let telegramSignals = {
   stopSignal: false,
   exitSignal: false,
@@ -282,10 +282,15 @@ let api = new Api({});
 
 let limits;
 getEMAQtyForGeneric = () => {
+  // return debug ? 100 : 
+  // limits?.cash < 800000 ? 
+  //   [100, 75, 240, 75, 150, 70, 75][new Date().getDay()] : 
+  //   [400, 525, 1600, 525, 1050, 490, 525][new Date().getDay()]
+
   return debug ? 100 : 
   limits?.cash < 800000 ? 
-    [100, 75, 240, 75, 150, 70, 75][new Date().getDay()] : 
-    [400, 525, 1600, 525, 1050, 490, 525][new Date().getDay()]
+    [100, 75, 150, 240, 75, 70, 75][new Date().getDay()] : 
+    [400, 525, 1000, 1560, 505, 490, 525][new Date().getDay()]
   }
 
 // Execute the findNearestExpiry function
