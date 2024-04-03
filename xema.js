cee1478bd1041d9d038eaa6742308bc4a54bc8f2
Pawan Kumar Jha <88pawankumarjha@gmail.com>
2024-04-03 04:16:55 +0000

    more lots

--- xema.js
+++ xema.js
@@ -284,13 +284,13 @@ let limits;
 getEMAQtyForGeneric = () => {
   // return debug ? 100 : 
   // limits?.cash < 800000 ? 
-  // [100, 300, 1000, 300, 800, 300, 75][new Date().getDay()] : 
+  // [100, 75, 240, 75, 200, 70, 75][new Date().getDay()] : 
   // [400, 525, 1600, 525, 1050, 490, 525][new Date().getDay()]
 
   return debug ? 100 : 
   limits?.cash < 1500000 ? 
-  [100, 75, 240, 75, 200, 70, 75][new Date().getDay()] : 
-  [100, 540, 1600, 540, 1700, 550, 75][new Date().getDay()]
+  [100, 300, 1000, 300, 800, 300, 75][new Date().getDay()] : 
+  [100, 540, 1600, 750, 1700, 550, 75][new Date().getDay()]
   }
 
 // Execute the findNearestExpiry function
