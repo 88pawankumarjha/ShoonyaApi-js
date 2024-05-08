ff2548739d4829a639fda84f4fad239ac4d834b8
Pawan Kumar Jha <88pawankumarjha@gmail.com>
2024-05-08 04:07:59 +0000

    qty fix

--- xema.js
+++ xema.js
@@ -292,7 +292,7 @@ getEMAQtyForGeneric = () => {
 
   return debug ? 100 : 
   limits?.cash < 2000000 ? 
-   [100, 420, 1200, 520, 1200, 300, 75][new Date().getDay()] : 
+   [100, 420, 1200, 510, 1200, 300, 75][new Date().getDay()] : 
    [100, 960, 2600, 960, 1800, 700, 75][new Date().getDay()]
   // bnf early expiry
   // [100, 300, 300, 300, 800, 250, 75][new Date().getDay()] : 
